7c042d3d3a9ab02d2e6b67b9ba304c1e22355070
Ruben Paz <me@ruben.io>
2015-09-03 17:28:00 +0200

    Added fire helper files

---
+++ FireHelperAPI/src/java/com/alcist/firehelper/FireHelper.java
@@ -0,0 +1,15 @@
+package com.alcist.firehelper;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by istar on 04/08/14.
+ */
+public interface FireHelper {
+
+    /**
+     * Returns a instance of Firebase
+     * @return an instance of Firebase
+     */
+    Firebase getFirebase();
+}
---
+++ src/java/eu/anvilindustries/anvilcraft/firehelper/Plugin.java
@@ -0,0 +1,41 @@
+package eu.anvilindustries.anvilcraft.firehelper;
+
+import com.firebase.client.Firebase;
+import com.alcist.firehelper.FireHelper;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by istar on 04/08/14.
+ */
+public class Plugin extends JavaPlugin implements FireHelper {
+
+    Firebase firebase;
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        setConfig();
+        String url = getConfig().getString("url");
+        if(url.equals("https://default.firebaseio.com")) {
+            getLogger().info("You have to change the firebase base url in the config.");
+            getPluginLoader().disablePlugin(this);
+        }
+        getLogger().info("Firebase url: " + url);
+        firebase = new Firebase(url);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        firebase = null;
+    }
+
+    public void setConfig() {
+        saveResource("config.yml", false);
+    }
+
+    @Override
+    public Firebase getFirebase() {
+        return firebase;
+    }
+}
