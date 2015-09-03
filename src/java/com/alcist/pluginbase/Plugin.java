b8f723cbf42789eb5dbc5e4a9047cd45c2a40533
Ruben Paz <me@ruben.io>
2015-09-03 13:58:36 +0200

    first commit

---
+++ src/java/com/alcist/pluginbase/Plugin.java
@@ -0,0 +1,20 @@
+package com.alcist.pluginbase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by istar on 03/08/14.
+ */
+public class Plugin extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
